1b55c389c76968be0a8c3e09e1fcc915c2a99234
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-02-10 05:10:32 +0000

    send email function changed for better user experience

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,14 +2,17 @@ function sendMail(contactForm) {
     emailjs.send("service_dhbb9pk", "template_6e1jo6f", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "message": contactForm.projectsummary.value
+
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            alert("Your project request has been sent. We will get back to you soon!");
         },
         function(error) {
             console.log("FAILED", error);
+            alert("There was an error sending your project request. Please try again later.");
         }
     );
     return false;  // To block from loading a new page
